a71f54fed855a0de64baa15228e0afac6a6cb27d
hckerhx <hangzuoxiang@gmail.com>
2018-04-06 17:48:50 -0400

    Solver version 2.0

--- Solver.m
+++ Solver.m
@@ -1,4 +1,4 @@
-[x, fval] = Solver(xi, b, G);
+function [x, fval] = Solver(xi, b, G);
 
 %number of scenarios
 [r, c] = size(xi);
@@ -7,10 +7,10 @@
 obj = [zeros(1, 20+r*20), repmat([5, -10], 1, r)]./r;
 
 %first constraint
-FC = [ones(1,20), zeros(1,r*23)];
+FC = [ones(1,20), zeros(1,r*22)];
 
 %second constraint 
-SC = zeros(3, 20+20*r+3*r);
+SC = zeros(r, 20+22*r);
 
 for i = 1:r
     SC(i,1:20) = xi(i,:);
@@ -18,11 +18,11 @@ for i = 1:r
 end
 
 %third constraint
-TC = zeros(3, 20+20*r+3*r);
+TC = zeros(r, 20+22*r);
 
 for i = 1:r
     TC(i,20*i+1:20+20*i) = xi(i,:);
     TC(i,19+20*r+2*i:19+20*r+2*i+1) = [-1,1];
 end
 
-[x, fval] = linprog(-obj, [], [], [FC;SC;TC], [b; zeros(r,1); repmat(5,[r,1])], zeros(20+23*r,1),[]);
+[x, fval] = linprog(-obj, [], [], [FC;SC;TC], [b; zeros(r,1); repmat(5,[r,1])], zeros(20+22*r,1),[]);
